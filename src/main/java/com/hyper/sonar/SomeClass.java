72d63a6df47a6c560ce577e1316a720fea7f1d53
fariburz <ebrahimi@adtn.ir>
2015-08-09 13:06:10 +0430

    commit

---
+++ src/main/java/com/hyper/sonar/SomeClass.java
@@ -0,0 +1,22 @@
+package com.hyper.sonar;
+
+
+public class SomeClass {
+	
+	public SomeClass() {
+		
+	}
+	
+	public void printHyper(){
+		System.out.println("Hyper");
+		
+	}
+	
+	public String printHyper(String text){
+		System.out.println(text);
+		return "success";
+		
+	}
+	
+
+}
