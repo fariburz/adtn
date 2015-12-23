8a63b79861d129644a7de736ec5f68508bf14157
fariborz <fariborz@hyperoffice.com>
2015-12-23 17:32:08 +0330

    new

--- src/main/java/com/hyper/sonar/SomeClass.java
+++ src/main/java/com/hyper/sonar/SomeClass.java
@@ -56,7 +56,7 @@ public class SomeClass {
 
 			}
 			// STEP 6: Clean-up environment
-			// rs.close();
+			 rs.close();
 			 stmt.close();
 			 conn.close();
 		} catch (SQLException se) {
@@ -65,7 +65,24 @@ public class SomeClass {
 		} catch (Exception e) {
 			// Handle errors for Class.forName
 			e.printStackTrace();
-		} 
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null) {
+				stmt.close();
+				}
+
+			} catch (SQLException se2) {
+			}// nothing we can do
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+				
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}// end finally try
+		}// end try
 		System.out.println("Goodbye!");
 	}// end main
 }// end FirstExample
